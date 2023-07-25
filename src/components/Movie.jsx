ec69c183768ff7833867e2b7e0205884e8485d47
Myroslava Shevska <miroslavashevskaya12@gmail.com>
2023-07-25 12:53:53 +0200

    Update

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -10,15 +10,14 @@ function Movie(props) {
 
     return <div id = {id} className = "card movie">
         <div className = "card-image waves-effect waves-block waves-light">
-            {
-                poster === 'N/A' ?
-                    (<img className = "activator"
-                          src = {'https://via.placeholder.com/300x430?text=${title}'}/>
-                    )
-                    :
-                    (<img className = "activator" src = {poster}/>)
-            }
-
+            {poster === 'N/A' ? (
+                <img
+                    className = "activator"
+                    src = {`https://via.placeholder.com/300x400?text=${title}`}
+                />
+            ) : (
+                <img className = "activator" src = {poster}/>
+            )}
         </div>
         <div className = "card-content">
             <span className = "card-title activator grey-text text-darken-4">{title}</span>
